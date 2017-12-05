4f538c33bd2e1b631ef5d235787b0ce0960dfcbe
Daniel Lobaton <dlobaton2@gmail.com>
2017-12-05 12:36:37 -0500

    base behaviors

--- src/ufl/cs1/controllers/StudentController.java
+++ src/ufl/cs1/controllers/StudentController.java
@@ -1,5 +1,7 @@
 package ufl.cs1.controllers;
 
+
+
 import game.controllers.DefenderController;
 import game.models.Defender;
 import game.models.Game;
@@ -12,22 +14,123 @@ public final class StudentController implements DefenderController
 
 	public void shutdown(Game game) { }
 
-	public int[] update(Game game,long timeDue)
+	public int[] update(Game game, long timeDue)
 	{
 		int[] actions = new int[Game.NUM_DEFENDER];
 		List<Defender> enemies = game.getDefenders();
-		
-		//Chooses a random LEGAL action if required. Could be much simpler by simply returning
-		//any random number of all of the ghosts
-		for(int i = 0; i < actions.length; i++)
-		{
-			Defender defender = enemies.get(i);
-			List<Integer> possibleDirs = defender.getPossibleDirs();
-			if (possibleDirs.size() != 0)
-				actions[i]=possibleDirs.get(Game.rng.nextInt(possibleDirs.size()));
-			else
-				actions[i] = -1;
-		}
+
+		actions[0] = uniqueBehavior1(game, enemies.get(0), timeDue);
+		//actions[0] = 1;
+		actions[1] = uniqueBehavior2(game, enemies.get(1), timeDue);
+		actions[2] = uniqueBehavior3(game, enemies.get(2), timeDue);
+		actions[3] = uniqueBehavior4(game, enemies.get(3), timeDue);
 		return actions;
+	}
+
+	public int uniqueBehavior1(Game game, Defender defender, long timeDue)
+	{
+		int direction = 0;
+		int attackerX = game.getAttacker().getLocation().getX();
+		int attackerY = game.getAttacker().getLocation().getY();
+		int defenderX = defender.getLocation().getX();
+		int defenderY = defender.getLocation().getY();
+		int differenceY = Math.abs(attackerY - defenderY);
+		int differenceX = Math.abs(attackerX - defenderX);
+
+		if(!defender.isVulnerable())
+		{
+			if(differenceY > differenceX)
+			{
+				if(attackerY > defenderY)
+				{
+					direction = 2;
+				}
+				else if(attackerY < defenderY)
+				{
+					direction = 0;
+				}
+			}
+			else if(differenceY < differenceX)
+			{
+				if(attackerX > defenderX)
+				{
+					direction = 1;
+				}
+				else if(attackerX < defenderX)
+				{
+					direction = 3;
+				}
+			}
+		}
+		else if(defender.isVulnerable())
+		{
+			if(differenceY > differenceX)
+			{
+				if(attackerY > defenderY)
+				{
+					direction = 0;
+				}
+				else if(attackerY < defenderY)
+				{
+					direction = 2;
+				}
+			}
+			else if(differenceY < differenceX)
+			{
+				if(attackerX > defenderX)
+				{
+					direction = 3;
+				}
+				else if(attackerX < defenderX)
+				{
+					direction = 1;
+				}
+			}
+		}
+		return direction;
+
+
+	}
+	public int uniqueBehavior2(Game game, Defender defender, long timeDue)
+	{
+		int direction = 0;
+		List<Integer> possibleDirs = defender.getPossibleDirs();
+		if(possibleDirs.size() != 0)
+		{
+			direction = possibleDirs.get(Game.rng.nextInt(possibleDirs.size()));
+		}
+		else
+		{
+			direction = -1;
+		}
+		return direction;
+	}
+	public int uniqueBehavior3(Game game, Defender defender, long timeDue)
+	{
+		int direction = 0;
+		List<Integer> possibleDirs = defender.getPossibleDirs();
+		if(possibleDirs.size() != 0)
+		{
+			direction = possibleDirs.get(Game.rng.nextInt(possibleDirs.size()));
+		}
+		else
+		{
+			direction = -1;
+		}
+		return direction;
+	}
+	public int uniqueBehavior4(Game game, Defender defender, long timeDue)
+	{
+		int direction = 0;
+		List<Integer> possibleDirs = defender.getPossibleDirs();
+		if(possibleDirs.size() != 0)
+		{
+			direction = possibleDirs.get(Game.rng.nextInt(possibleDirs.size()));
+		}
+		else
+		{
+			direction = -1;
+		}
+		return direction;
 	}
 }
